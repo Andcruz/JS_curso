90feae9e67b8da328ec2f64b0b6d8b5e56650ae8
Anderson <andchavescruz@gmail>
2026-03-20 11:17:07 -0300

    condicionamento if e else

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 39
+console.log(`A velocidade maxíma permitida é de 100km/h, e a miníma é 40 km/h`)
+if (vel > 100) {
+    console.log(`você está a ${vel}km/h e será multado!`)
+}
+if(vel < 40) {
+    console.log(`Você está a ${vel} km/h, você será multado!`)
+}
