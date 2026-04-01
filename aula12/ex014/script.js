597655a13ca8a94abd168278aa83184d96b5d486
Anderson <andchavescruz@gmail.com>
2026-04-01 20:03:30 -0300

    aula12 projeto web

---
+++ aula12/ex014/script.js
@@ -0,0 +1,24 @@
+function carregar(){
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+hora = 19
+//var hora = data.getHours()
+msg.innerHTML = `Agora são ${hora} horas.`
+if (hora >= 0 && hora < 12) {
+    //BOM DIA!
+    img.src = 'amanhecer.png'
+    document.body.style.background = '#dac7b8'
+    msg2.innerHTML = 'BOM DIA!'
+} else if (hora >= 12 && hora < 18) {
+    // BOA TARDE!
+    img.src = 'entardecer.png'
+    document.body.style.background = '#cd4b00ff'
+    msg2.innerHTML = 'BOA TARDE!'
+} else {
+    //BOA NOITE!
+    img.src = 'anoitecer.png'
+    document.body.style.background = '#001125'
+    msg2.innerHTML = 'BOA NOITE!'
+}
+}
