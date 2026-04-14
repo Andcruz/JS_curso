8e1280d2cd97cbf499d5aed1289e82642e21b4e2
Anderson <andchavescruz@gmail.com>
2026-04-14 13:39:04 -0300

    ex17

--- aula14/ex17/tabuada.js
+++ aula14/ex17/tabuada.js
@@ -1,14 +1,15 @@
-function gerarTab() {
-    let qnt = document.getElementById('txt1')
-    //let qnt2 = document.getElementById('txt2')
-    let res = document.getElementById('res')
-    /*if (res.value > 1) {
-        window.alert('⚠️ Você já tem sua resposta!')
-    }*/
-    let q = Number (qnt.value)
-   // let q2 = Number (qnt2.value)
-    for ( i = 1; i <= 10; i ++) {
-        let mult = q * i
-        res.innerHTML += (`${q} X ${i} = ${mult} <br>`) 
+function tabuada() {
+    let num = document.getElementById('txt1')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('⚠️ Digite um número!') 
+    } else {      
+        let n = Number (num.value)
+        tab.innerHTML = ''
+        for (let i = 1; i <= 10; i ++) {
+            let item = document.createElement('option')
+            item.text = (`${n} X ${i} = ${n*i}`) 
+            tab.appendChild(item) 
+        }
     }        
 }
