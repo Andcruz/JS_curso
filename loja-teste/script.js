9952544bc8ed334b4459560d8be8a1790fd54bae
Anderson <andchavescruz@gmail.com>
2026-04-15 13:01:06 -0300

    Loja Teste: inserção de imagens

--- loja-teste/script.js
+++ loja-teste/script.js
@@ -1,27 +1,30 @@
 let  produtos = document.getElementById("produtos")
-console.log("JS funcionando")
 
 let camisas = [
-    {time:'Palmeiras', preco:'R$ 199,00'},
-    {time:'Flamengo', preco:'199,00'},
-    {time:'Barcelona', preco:'399,00'}
+    {time:'Palmeiras 25/26 Torcedor', preco:'R$ 199,00', imagem:'img/palmeiras1.jpg'},
+    {time:'Flamengo 25/26 Torcedor', preco:'199,00', imagem:'img/flamengo1.jpg'},
+    {time:'Barcelona 25/26 Torcedor', preco:'399,00', imagem:'img/barça1.jpg'}
 ]
 
 camisas.forEach(camisa => {
 
     let card = document.createElement("div")
     card.className = "card"
-
-    let nome = document.createElement("h3")
+//nome do time
+    let nome = document.createElement("h4")
     nome.innerText = camisa.time
-
+    //Imagem das camisas
+        let img = document.createElement("img")
+        img.src = camisa.imagem
+        img.alt = camisa.time
+//Preço da camisa
     let preco = document.createElement("p")
     preco.innerText = camisa.preco
-
+//Botão de compra
     let botao = document.createElement("button")
     botao.innerText = "Comprar"
 
-    card.append(nome, preco, botao)
+    card.append(nome, img, preco, botao)
 
     produtos.append(card)
 })
